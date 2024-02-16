b9a8e83dace566cd5c17dba20976317afd69d14f
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-16 11:05:35 +0100

    Add project files.

---
+++ Uppgift_6.12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift_6_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RitaTriangel(3);
+            RitaTriangel(4);
+        }
+        static void RitaTriangel(int höjd)
+        {
+            for (int i = 0; i < höjd; i++) //Rader
+            {
+                for (int j = 0; j < höjd - 1 - i; j++) //Tomrummet innan raden
+                {
+                    Console.Write(' ');
+                }
+
+                for (int j = 0; j < 2*i + 1; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
